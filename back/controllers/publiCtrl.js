5d1e2e2edf42fed836ef37ebca5abb4c64a54ad1
nico <laportenicolas@protonmail.com>
2021-04-08 16:13:55 +0200

    route/conttroler getAllPubli/GetOnePubli Publication

--- back/controllers/publiCtrl.js
+++ back/controllers/publiCtrl.js
@@ -22,3 +22,22 @@ exports.edit            = (req, res) => {
                 .catch (err => res.status(400).json({err}));
         })
 }
+
+exports.getAllPubli     = (req, res) => {
+    connexion()
+        .then(con => {
+            con.query("SELECT * FROM publication")
+                .then (rows => res.status(200).json(rows['0']))
+                .catch (err => res.status(400).json({err}));
+        })
+        .catch (err => res.status(400).json({err}))
+}
+
+exports.getOnePubli     = (req, res) => {
+    connexion()
+        .then(con => {
+            con.query("SELECT * FROM publi WHERE id = ?", [req.params.id])
+                .then(row => res.status(200).json(row['0']))
+                .catch(err => res.status(400).json({err}))
+        })
+}
--- back/routes/publiRoute.js
+++ back/routes/publiRoute.js
@@ -6,5 +6,7 @@ const router    = express.Router();
 
 router.post('/new', auth, multer, ctrlPubli.new);
 router.put('/edit', auth, multer, ctrlPubli.new);
+router.get('/getAll', auth, ctrlPubli.getAllPubli);
+router.get('/getOne/:id', auth, ctrlPubli.getOnePubli);
 
 module.exports  = router;
