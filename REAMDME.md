66ad3ecd66299290392dab1af54295638120e6a5
nico <laportenicolas@protonmail.com>
2021-04-20 17:33:20 +0200

    Utilisation des var d'env pour la base de données

--- REAMDME.md
+++ REAMDME.md
@@ -2,8 +2,12 @@ NodeJS version 14.16
 
 Pour le dossier back
   - npm install
-  - nodemon server ou node server
+  - npm start
   
-Pourt le dossier front
+Pour le dossier front
   - yarn install
   - yarn start 
+
+Pour le base de données
+    - Configurer /back/package.json:8
+    - Remplacer "cross-env APP_DB_HOST= APP_DB_USERNAME= APP_DB_PASSWORD= APP_DB_DATABASE= " par les identifiant de la base de données
--- back/bdd.js
+++ back/bdd.js
@@ -1,9 +1,16 @@
 const mariadb       = require('mariadb');
+const {
+    APP_DB_HOST,
+    APP_DB_USERNAME,
+    APP_DB_PASSWORD,
+    APP_DB_DATABASE
+} = process.env
+
 const connection    = () => mariadb.createConnection({
-    host: '127.0.0.1',
-    user: 'groupomania',
-    password: 'password',
-    database: 'Groupomania'
+    host: APP_DB_HOST,
+    user: APP_DB_USERNAME,
+    password: APP_DB_PASSWORD,
+    database: APP_DB_DATABASE
 })
 
 module.exports      = connection;
